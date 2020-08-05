aa2b0c1329e6705f762779380bae9c57e025af36
Einar Örn Gissurarson <einarog05@ru.is>
2020-08-05 17:53:20 +0000

    Initial 1.0 version
    First working copy of the Canvas Maker.
    
    Missing validation checks and uses practice_exam and short_answer_questions as a default type of quiz/questions.

---
+++ quiz_maker.py
@@ -0,0 +1,124 @@
+# Author: Einar Örn Gissurarson (einarog05@ru.is)
+
+#Used in conjunction with config.txt, quiz_data.csv, and question_data.csv. Do not run without these files.
+
+import sys, os.path, csv, json, requests, itertools, collections
+
+if os.path.isfile('config.csv') and os.path.isfile('quiz_data.csv') and os.path.isfile('question_data.csv'):
+	print('Files present, attempting validation')
+	#TODO validation stuff
+else:
+	sys.exit("Please ensure the following files exist: config.txt, quiz_data.csv, question_data.csv")
+
+#Fetch Config data
+with open('config.csv' , newline='', encoding='utf-8-sig') as csvconfig:
+	config_reader = csv.DictReader(csvconfig, delimiter=';')
+	config_data = next(config_reader)
+
+headers = {'Authorization' : 'Bearer '+config_data["User_Access_token"]}
+url = 'https://reykjavik.instructure.com//api/v1/courses/'+config_data["Course_ID"]+'/quizzes'
+
+#Fetch quiz data
+with open('quiz_data.csv' , newline='', encoding='utf-8-sig') as csvquiz:
+	quiz_reader = csv.DictReader(csvquiz, delimiter=';')
+	quiz_CSV_data = next(quiz_reader)
+
+quiz_data = {
+	'quiz[title]' : quiz_CSV_data["title"],
+	'quiz[description]' : quiz_CSV_data["description"],
+	'quiz[quiz_type]' : quiz_CSV_data["quiz_type"],
+	'quiz[assignment_group_id]' : quiz_CSV_data["assignment_group_id"],
+	'quiz[time_limit]' : quiz_CSV_data["time_limit"],
+	'quiz[shuffle_answers]' : quiz_CSV_data["shuffle_answers"],
+	'quiz[hide_results]' : quiz_CSV_data["hide_results"],
+	'quiz[show_correct_answers]' : quiz_CSV_data["show_correct_answers"],
+	'quiz[show_correct_answers_last_attempt]' : quiz_CSV_data["show_correct_answers_last_attempt"],
+	'quiz[show_correct_answers_at]' : quiz_CSV_data["show_correct_answers_at"],
+	'quiz[hide_correct_answers_at]' : quiz_CSV_data["hide_correct_answers_a"],
+	'quiz[one_time_results]' : quiz_CSV_data["one_time_results"],
+	'quiz[allowed_attempts]' : quiz_CSV_data["allowed_attempts"],
+	'quiz[scoring_policy]' : quiz_CSV_data["scoring_policy"],
+	'quiz[one_question_at_a_time]' : quiz_CSV_data["one_question_at_a_time"],
+	'quiz[cant_go_back]' : quiz_CSV_data["cant_go_back"],
+	'quiz[access_code]' : quiz_CSV_data["access_code"],
+	'quiz[ip_filter]' : quiz_CSV_data["ip_filter"],
+	'quiz[due_at]' : quiz_CSV_data["due_at"],
+	'quiz[lock_at]' : quiz_CSV_data["lock_at"],
+	'quiz[unlock_at]' : quiz_CSV_data["unlock_at"],
+	'quiz[published]' : quiz_CSV_data["published"],
+	'quiz[only_visible_to_overrides]' : quiz_CSV_data["only_visible_to_overrides"]
+}
+
+r = requests.post(url, headers = headers, params = quiz_data)
+y = r.json()
+
+print('Quiz created - Output:')
+print(r.status_code)
+print(y)
+print('\n===========================================\n')
+
+quiz_id = y['id']
+
+group_data = {
+	'quiz_groups[][name]' : quiz_CSV_data["quiz_group_name"],
+	'quiz_groups[][pick_count]' : quiz_CSV_data["quiz_group_pick_count"],
+	'quiz_groups[][question_points]' : quiz_CSV_data["quiz_group_question_points"]
+}
+
+r = requests.post(url+'/'+str(quiz_id)+'/groups', headers = headers, params = group_data)
+y = r.json()
+group_id = y['quiz_groups'][0]['id']
+
+print('Quiz group created - Output:')
+print(r.status_code)
+print(y)
+print('\n===========================================\n')
+
+with open('question_data.csv' , newline='', encoding='utf-8-sig') as csvfile:
+
+	#Column reader used to determine maximum number of viable answers.	
+	column_reader, row_reader = itertools.tee(csv.reader(csvfile, delimiter=';'))
+	columns = len(next(column_reader))
+	del column_reader
+	
+	#question_pos used to give questions a valid position in list.
+	question_pos = 0
+	
+	#Ignore headers
+	next(row_reader)
+	for row in row_reader:
+		question_data = collections.defaultdict(list)
+
+		question_data['question[question_name]'].append(row[0])
+		question_data['question[question_text]'].append(row[1])
+		question_data['question[quiz_group_id]'].append(group_id)
+		question_data['question[question_type]'].append('short_answer_question')
+		question_data['question[position]'].append(question_pos)
+		question_data['question[points_possible]'].append(5.0)
+
+		#answer_pos used to give answers a valid position for each question.
+		answer_pos = 0
+
+		#Populate each question with valid answers. 
+		#If valid answers are fewer than maximum number of answers, columns should be blank and thus ignored.
+		iterator = range(2, columns, 3)
+		for n in iterator:
+			if row[n] is not '':
+				question_data['question[answers]['+str(answer_pos)+'][text]'].append(row[n])
+				question_data['question[answers]['+str(answer_pos)+'][comments]'].append(row[n+1])
+				question_data['question[answers]['+str(answer_pos)+'][comments_html]'].append('<p>'+row[n+1]+'</p>')
+				question_data['question[answers]['+str(answer_pos)+'][weight]'].append(row[n+2])
+
+				answer_pos += 1
+		question_pos += 1
+
+		#Post each question into canvas. Then delete data, so it can be repopulated.
+		r = requests.post(url+'/'+str(quiz_id)+'/questions', headers = headers, params = question_data)
+		print('Question #'+str(question_pos)+' created - Output:')
+		print(r.status_code)
+		print(r.json())
+		print('\n===========================================\n')
+		del question_data
+
+print('Done. Go to https://reykjavik.instructure.com/courses/'+config_data["Course_ID"]+'/question_banks where you can assign your questions to a question bank.')
+		
